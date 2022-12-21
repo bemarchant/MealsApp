4a59d7aae0aa2742e3ba837291d7ff6789f063fd
bemarchant <bemarchant@proton.me>
2022-12-21 15:36:29 -0300

    feat(app): managed case no favorite meals

--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { StyleSheet, View, Text } from "react-native";
 import { FavoritesContext } from "../store/context/favorites-context";
 import MealsList from "../components/MealList/MealsList";
 import { MEALS } from "../data/dummy-data";
@@ -9,7 +10,29 @@ const FavoriteScreen = () => {
     favoriteMealCtx.ids.includes(mealItem.id)
   );
 
+  if (favoriteMeals.length === 0) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>No favorite meals yet ...</Text>
+      </View>
+    );
+  }
   return <MealsList items={favoriteMeals} />;
 };
 
 export default FavoriteScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    margin: 20,
+    justifyContent: "center",
+    aligntItems: "center",
+  },
+
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
